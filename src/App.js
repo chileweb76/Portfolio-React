4fca959f7686a37d30d50947c405d2483c240f6a
“Christopher <christopherhileweb@gmail.com>
2023-02-11 20:02:06 -0500

    homepage work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+
+import { Route, Routes } from "react-router-dom";
 import HeaderLower from "./components/HeaderLower";
 import HeaderUpper from "./components/HeaderUpper";
+import NavLinks from "./components/NavLinks";
+import Homepage from "./pages/Homepage";
 
 
 function App() {
@@ -8,7 +12,12 @@ function App() {
         <HeaderUpper />
         <HeaderLower />
         <main>
-          
+          <NavLinks />
+          <Routes>
+            <Route path="/" element={<Homepage />}/>
+            <Route />
+            <Route />
+          </Routes>
         </main>
     </>
   );
--- src/components/HeaderUpper.js
+++ src/components/HeaderUpper.js
@@ -1,4 +1,5 @@
 import { Twitter, Linkedin } from 'react-bootstrap-icons'
+
 import dragonUpper from '../img/dragonbanner_top_web.png'
 
 
@@ -7,8 +8,8 @@ const HeaderUpper = () => {
 
     return (
         <header>
-            <a  href='https://twitter.com/chrishileweb'><Twitter className='social' /></a>
-            <a  href='https://www.linkedin.com/in/christopher-hile/'><Linkedin className='social' /></a>
+            <a href='https://twitter.com/chrishileweb'><Twitter className='social' /></a>
+            <a href='https://www.linkedin.com/in/christopher-hile/'><Linkedin className='social' /></a>
             <h1>Christopher Hile</h1>
             <img src={dragonUpper} alt="Dragon Upper" />
         </header>
